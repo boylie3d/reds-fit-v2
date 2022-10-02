097bf25dcdf59af668df2a21361125a1a4b72e2d
Dave Boyle <dave@boylie3d.com>
2022-10-02 14:21:47 -0400

    sign in box
    layout sorted, doesn't do anything, need to set up api endpoints

--- components/loginBox.tsx
+++ components/loginBox.tsx
@@ -1,6 +1,7 @@
 import { EmailIcon, LockIcon } from "@chakra-ui/icons"
 import {
   Box,
+  Button,
   Icon,
   Input,
   InputGroup,
@@ -14,11 +15,12 @@ import {
   VStack,
 } from "@chakra-ui/react"
 import { useState } from "react"
+import { SubmitHandler, useForm } from "react-hook-form"
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
 
 export default function LoginBox() {
   return (
-    <Box borderWidth="2px" borderRadius="lg" h="20em" w="25em" pb="2em">
+    <Box borderWidth="2px" borderRadius="lg" h="18em" w="25em" pb="2em">
       <Tabs isFitted variant="enclosed">
         <TabList mb="1em">
           <Tab>Login</Tab>
@@ -37,8 +39,21 @@ export default function LoginBox() {
   )
 }
 
+type FormInput = {
+  email: string
+  pass: string
+}
+
 const Login = () => {
   const [show, setShow] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormInput>()
+
+  const onSubmit: SubmitHandler<FormInput> = data => console.log(data)
 
   const PasswordShow = () => {
     if (show) return <AiOutlineEyeInvisible color="gray" />
@@ -46,28 +61,44 @@ const Login = () => {
   }
 
   return (
-    <VStack>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <VStack spacing="1em">
+        <InputGroup>
+          <InputLeftElement>
+            <EmailIcon color="gray.300" />
+          </InputLeftElement>
+          <Input placeholder="Email" {...register("email")} />
+        </InputGroup>
+        <InputGroup>
+          <InputLeftElement>
+            <LockIcon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            placeholder="Password"
+            type={show ? "text" : "password"}
+            {...register("pass")}
+          />
+          <InputRightElement>
+            <Box onClick={() => setShow(!show)}>
+              <Icon as={PasswordShow} />
+            </Box>
+          </InputRightElement>
+        </InputGroup>
+        <Button type="submit">Sign in</Button>
+      </VStack>
+    </form>
+  )
+}
+
+const SignUp = () => {
+  return (
+    <VStack spacing="1em">
       <InputGroup>
         <InputLeftElement>
           <EmailIcon color="gray.300" />
         </InputLeftElement>
         <Input placeholder="Email" />
       </InputGroup>
-      <InputGroup>
-        <InputLeftElement>
-          <LockIcon color="gray.300" />
-        </InputLeftElement>
-        <Input placeholder="Password" type={show ? "text" : "password"} />
-        <InputRightElement>
-          <Box onClick={() => setShow(!show)}>
-            <Icon as={PasswordShow} />
-          </Box>
-        </InputRightElement>
-      </InputGroup>
     </VStack>
   )
-}
-
-const SignUp = () => {
-  return <>sign up</>
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -3,10 +3,13 @@ import {
   Center,
   Divider,
   Flex,
+  Heading,
+  Icon,
   Image,
   Text,
   VStack,
 } from "@chakra-ui/react"
+import { FcGoogle } from "react-icons/fc"
 import LoginBox from "../components/loginBox"
 
 export default function Login() {
@@ -14,14 +17,18 @@ export default function Login() {
     <>
       <Center>
         <VStack>
-          <Image src="reds-logo.png" p="2em" />
+          <Image src="reds-logo.png" pt="2em" pr="2em" pl="2em" />
+          <Heading size="xl">REDS FIT</Heading>
           <LoginBox />
           <Flex align="center">
             <Divider orientation="horizontal" />
             <Text padding="2">OR</Text>
             <Divider orientation="horizontal" />
           </Flex>
-          <Button>Log In With Google</Button>
+          <Button>
+            <Icon h={6} w={6} as={FcGoogle} mr="0.5em" />
+            Log In With Google
+          </Button>
         </VStack>
       </Center>
     </>
