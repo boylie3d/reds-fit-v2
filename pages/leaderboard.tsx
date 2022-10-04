43448c486f5a4edd6f4ee97ae161d2a4d156671c
Dave Boyle <dave@boylie3d.com>
2022-10-04 13:44:36 -0400

    Update leaderboard.tsx

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -3,7 +3,7 @@ import AppLayout from "components/layout/appLayout"
 export default function LeaderboardPage() {
   return (
     <AppLayout>
-      <div>{"i'm in leaderboard"}</div>
+      <div>{"i'm in leaderboard!"}</div>
     </AppLayout>
   )
 }
