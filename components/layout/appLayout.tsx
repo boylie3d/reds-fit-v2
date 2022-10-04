f53be5683d51110ed11a1b58900bcce692e6e3aa
Dave Boyle <dave@boylie3d.com>
2022-10-03 22:11:43 -0400

    Update appLayout.tsx

--- components/layout/appLayout.tsx
+++ components/layout/appLayout.tsx
@@ -15,8 +15,9 @@ interface LayoutProps {
 const tmpUser: Profile = {
   firstName: "Dave",
   lastName: "Boyle",
+  displayName: "Dave Boyle",
   email: "dave@globacore.com",
-  profilePhoto:
+  photoURL:
     "https://pbs.twimg.com/profile_images/422249828500779009/rv2DKary_400x400.jpeg",
   userType: UserType.Admin,
 }
