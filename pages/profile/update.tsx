43b5a233ab2699c4bcbfff910ad69e76f3045730
Dave Boyle <dave@boylie3d.com>
2022-10-03 23:00:08 -0400

    more fixes

--- pages/profile/update.tsx
+++ pages/profile/update.tsx
@@ -2,9 +2,12 @@ import { Box } from "@chakra-ui/react"
 import ProfileForm from "components/profile/profileForm"
 
 export default function ProfileUpdate() {
+  //TODO: Remove
+  const tmpId = "UpKYkVJKXzQww0HiE88q6nD65mP2"
+
   return (
     <Box>
-      <ProfileForm />
+      <ProfileForm id={tmpId} />
     </Box>
   )
 }
--- test.ts
+++ test.ts
@@ -1,10 +1,12 @@
-import { Profile, UserType } from "./types"
+import { AccessType, Profile, UserType } from "./types"
 
 export const tmpUser: Profile = {
   firstName: "Dave",
   lastName: "Boyle",
+  displayName: "Dave Boyle",
   email: "dave@globacore.com",
-  profilePhoto:
+  photoURL:
     "https://pbs.twimg.com/profile_images/422249828500779009/rv2DKary_400x400.jpeg",
-  userType: UserType.Admin,
+  userType: UserType.Player,
+  accessType: AccessType.Admin,
 }
