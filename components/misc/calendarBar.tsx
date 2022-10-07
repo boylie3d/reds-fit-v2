a3adfc1206dd32b15ad53cda12d062511dd8ed38
Dave Boyle <dave@boylie3d.com>
2022-10-07 00:07:22 -0400

    added calendar stuff

--- components/misc/calendarBar.tsx
+++ components/misc/calendarBar.tsx
@@ -1,6 +1,18 @@
 import { ArrowLeftIcon, ArrowRightIcon, CalendarIcon } from "@chakra-ui/icons"
-import { Flex, HStack, Spacer, Text } from "@chakra-ui/react"
+import {
+  Box,
+  Flex,
+  HStack,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  Text,
+} from "@chakra-ui/react"
 import { useState } from "react"
+import { Calendar } from "react-calendar"
 import { addDays } from "util/time"
 
 interface CalendarProps {
@@ -10,6 +22,7 @@ interface CalendarProps {
 
 export default function CalendarBar({ date, onChanged }: CalendarProps) {
   const [currDate, setDate] = useState<Date>(date ? date : new Date())
+  const [calOpen, setCalOpen] = useState<boolean>(false)
 
   const nextDay = () => {
     const newDate = addDays(currDate, 1)
@@ -23,16 +36,65 @@ export default function CalendarBar({ date, onChanged }: CalendarProps) {
     if (onChanged) onChanged(currDate)
   }
 
+  const openCalendar = () => {}
+
   return (
     <Flex w="100%">
-      <ArrowLeftIcon onClick={previousDay} />
+      <ArrowLeftIcon
+        style={{
+          cursor: "pointer",
+        }}
+        onClick={previousDay}
+      />
       <Spacer />
-      <HStack gap={1}>
-        <CalendarIcon />
-        <Text>{currDate.toDateString()}</Text>
-      </HStack>
+      <Box
+        onClick={() => setCalOpen(true)}
+        style={{
+          cursor: "pointer",
+        }}
+      >
+        <HStack gap={1}>
+          <CalendarIcon />
+          <Text>{currDate.toDateString()}</Text>
+        </HStack>
+      </Box>
       <Spacer />
-      <ArrowRightIcon onClick={nextDay} />
+      <ArrowRightIcon
+        style={{
+          cursor: "pointer",
+        }}
+        onClick={nextDay}
+      />
+      <CalendarModal
+        open={calOpen}
+        date={currDate}
+        onClose={() => setCalOpen(false)}
+        onChange={newDate => setDate(newDate)}
+      />
     </Flex>
+  )
+}
+
+interface ModalProps {
+  open: boolean
+  onChange: (date: Date) => void
+  onClose: () => void
+  date: Date
+}
+
+const CalendarModal = ({ open, onChange, onClose, date }: ModalProps) => {
+  return (
+    <Modal isOpen={open} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Select Date</ModalHeader>
+        <ModalBody>
+          <Calendar
+            defaultValue={date}
+            onChange={(date: Date) => onChange(new Date(date))}
+          />
+        </ModalBody>
+      </ModalContent>
+    </Modal>
   )
 }
