682c2b8651232892a6718a2ad1e1cb52615965bc
Dave Boyle <dave@boylie3d.com>
2022-10-17 10:39:37 -0400

    announcements working

--- components/admin/announcement/announcementManagement.tsx
+++ components/admin/announcement/announcementManagement.tsx
@@ -1,21 +1,31 @@
 import { Announcement } from "@/types"
+import { CloseIcon } from "@chakra-ui/icons"
 import {
+  Box,
   Button,
+  Center,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Text,
   Textarea,
   useDisclosure,
   VStack,
 } from "@chakra-ui/react"
+import Card from "components/layout/card"
 import CalendarBar from "components/misc/calendarBar"
 import { useAnnouncements } from "hooks/announcement"
 import { NextPage } from "next"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FieldValues, useForm } from "react-hook-form"
+import { BsFillMegaphoneFill } from "react-icons/bs"
 import { useSWRConfig } from "swr"
 import { toUntimedDate } from "util/time"
 
@@ -28,15 +38,58 @@ const AnnouncementManagement: NextPage<Props> = ({}) => {
   })
   const { isOpen, onOpen, onClose } = useDisclosure()
   const { mutate } = useSWRConfig()
+  const [currAnnouncement, setCurrAnnouncement] = useState<
+    Announcement | undefined
+  >(undefined)
 
+  useEffect(() => {
+    setCurrAnnouncement(undefined)
+  }, [calDate, announcements])
+
+  //TODO: this isn't updating properly on create / delete - fixez
   const create = async (announcement: Announcement) => {
     const resp = await fetch("/api/announcement", {
       method: "POST",
       body: JSON.stringify(announcement),
     })
+    await resp.json()
 
-    const json = await resp.json()
-    mutate("/api/workout")
+    const options = { optimisticData: announcement, rollbackOnError: true }
+    mutate(`/api/announcement/${announcement.id}`, options)
+
+    closeModal()
+  }
+
+  const del = async (announcement: Announcement) => {
+    const resp = await fetch(`/api/announcement/${announcement.id}`, {
+      method: "DELETE",
+    })
+    await resp.json()
+    closeModal()
+  }
+
+  const openEditPanel = async (announcement: Announcement) => {
+    setCurrAnnouncement(announcement)
+    onOpen()
+  }
+
+  const update = async (announcement: Announcement) => {
+    const resp = await fetch(`/api/announcement/${announcement.id}`, {
+      method: "PUT",
+      body: JSON.stringify(announcement),
+    })
+    await resp.json()
+
+    const options = { optimisticData: announcement, rollbackOnError: true }
+    mutate("/api/announcement", options)
+
+    closeModal()
+  }
+
+  const closeModal = () => {
+    mutate("/api/announcement/")
+    setCalDate(new Date(calDate))
+    setCurrAnnouncement(undefined)
     onClose()
   }
 
@@ -45,46 +98,104 @@ const AnnouncementManagement: NextPage<Props> = ({}) => {
       <VStack gap={3}>
         <CalendarBar date={calDate} onChanged={setCalDate} />
         {announcements && announcements.length > 0 ? (
-          <div>{"there's stuff"}</div>
+          <Box w="100%">
+            {announcements.map(a => (
+              <AnnouncementItem
+                key={a.id}
+                announcement={a}
+                onDelete={() => del(a)}
+                onEdit={() => openEditPanel(a)}
+              />
+            ))}
+          </Box>
         ) : (
-          <>
-            <Button w="100%" onClick={onOpen}>
-              Create New
-            </Button>
-            <Modal isOpen={isOpen} onClose={onClose}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>Create New</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                  <AnnouncementForm
-                    onSubmit={create}
-                    currentDate={toUntimedDate(calDate)}
-                  />
-                </ModalBody>
-              </ModalContent>
-            </Modal>
-          </>
+          <Button w="100%" onClick={onOpen}>
+            Create New
+          </Button>
         )}
       </VStack>
+      <Modal isOpen={isOpen} onClose={closeModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            {currAnnouncement ? "Edit Existing" : "Create New"}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <AnnouncementForm
+              onSubmit={create}
+              onUpdate={update}
+              currentDate={toUntimedDate(calDate)}
+              existing={currAnnouncement}
+            />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
-
 export default AnnouncementManagement
+
+interface AnnouncementProps {
+  announcement: Announcement
+  onEdit: () => void
+  onDelete: () => void
+}
+
+const AnnouncementItem = ({
+  announcement,
+  onEdit,
+  onDelete,
+}: AnnouncementProps) => {
+  return (
+    <Card>
+      <VStack align="left" gap={3}>
+        <Center>
+          <HStack gap={2}>
+            <Icon color="teamPrimary" as={BsFillMegaphoneFill} />
+            <Heading size="xs">{"Today's Announcement:"}</Heading>
+            <Icon color="teamPrimary" as={BsFillMegaphoneFill} />
+          </HStack>
+        </Center>
+        <Text fontSize="xs">{announcement.message}</Text>
+        <Flex w="100%" gap={3}>
+          <Button onClick={onEdit} variant="teamOutline" flex={1}>
+            Edit
+          </Button>
+          <Button onClick={onDelete} variant="unstyled">
+            <CloseIcon color="teamPrimary" />
+          </Button>
+        </Flex>
+      </VStack>
+    </Card>
+  )
+}
 
 interface FormProps {
   currentDate: string
   onSubmit: (announcement: Announcement) => void
+  onUpdate: (announcement: Announcement) => void
+  existing: Announcement | undefined
 }
 
-const AnnouncementForm = ({ onSubmit, currentDate }: FormProps) => {
+const AnnouncementForm = ({
+  onSubmit,
+  onUpdate,
+  currentDate,
+  existing,
+}: FormProps) => {
   const { register, handleSubmit } = useForm()
 
   const submit = (form: FieldValues) => {
+    console.log(existing === undefined)
+    if (existing === undefined) create(form.message)
+    else update(form.message)
+  }
+
+  const create = (message: string) => {
     const now = new Date()
     const announcement: Announcement = {
-      message: form.message,
+      message: message,
       live: currentDate,
       created: now,
       updated: now,
@@ -92,10 +203,21 @@ const AnnouncementForm = ({ onSubmit, currentDate }: FormProps) => {
     onSubmit(announcement)
   }
 
+  const update = (message: string) => {
+    existing!.message = message
+    existing!.updated = new Date()
+
+    onUpdate(existing!)
+  }
+
   return (
     <form onSubmit={handleSubmit(submit)}>
       <VStack gap={2}>
-        <Textarea {...register("message")} placeholder="Your announcement" />
+        <Textarea
+          {...register("message")}
+          placeholder="Your announcement"
+          defaultValue={existing ? existing.message : ""}
+        />
         <Button w="100%" type="submit">
           Submit
         </Button>
