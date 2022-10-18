2d267c11db6e4ce2635a180e9c945df828c688ac
Dave Boyle <dave@boylie3d.com>
2022-10-18 17:16:53 -0400

    blocked users working
    closes #39

--- components/auth/authProvider.tsx
+++ components/auth/authProvider.tsx
@@ -1,8 +1,10 @@
+import { AccessType } from "@/types"
 import { useLocalProfile } from "hooks/profile"
 import { useRouter } from "next/router"
 import { useEffect } from "react"
 import { useAuthState } from "react-firebase-hooks/auth"
 import fb from "util/firebase"
+import UserBlock from "./userBlock"
 
 interface AuthProps {
   children: JSX.Element[] | JSX.Element
@@ -26,5 +28,10 @@ export default function AuthProvider(props: AuthProps) {
   }, [loading, profile])
 
   if (userLoading || loading) return <div />
+
+  if (profile?.accessType === AccessType.Blocked) {
+    return <UserBlock />
+  }
+
   return <div>{props.children}</div>
 }
