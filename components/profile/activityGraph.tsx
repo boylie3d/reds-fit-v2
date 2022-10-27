4937b149ee967056ad32799f73c73b42210f6346
Dave Boyle <dave@boylie3d.com>
2022-10-26 20:39:11 -0400

    activity stats working
    holy shit i got it

---
+++ components/profile/activityGraph.tsx
@@ -0,0 +1,91 @@
+import { Result } from "@/types"
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  ChartData,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from "chart.js"
+import { DateTime, Interval } from "luxon"
+import { useEffect, useState } from "react"
+import { Bar } from "react-chartjs-2"
+import { colors } from "theme"
+
+interface Props {
+  results: Result[]
+}
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+const options = {
+  responsive: true,
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+    },
+    y: {
+      ticks: {
+        precision: 0,
+      },
+      grid: {
+        display: false,
+      },
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+}
+
+const ActivityGraph = ({ results }: Props) => {
+  const [data, setData] = useState<ChartData<"bar"> | undefined>()
+
+  useEffect(() => {
+    const now = DateTime.now()
+    const before = now.minus({ weeks: 10 })
+    const interval = Interval.fromDateTimes(before, now)
+    const labels = interval
+      .splitBy({ week: 1 })
+      .map(d => `${d.end.monthShort} ${d.end.day}`)
+
+    const groups = interval.splitBy({ week: 1 }).map(g => {
+      const filter = results.filter(r => {
+        const date = DateTime.fromISO(r.created!.toString())
+        console.log("Label Day: ", g.end.toISODate())
+        console.log("Result Day: ", date.toISODate())
+        if (g.contains(date)) {
+          return r
+        }
+      })
+      return filter.length
+    })
+
+    const chartData: ChartData<"bar"> = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Results for Week",
+          data: groups,
+          backgroundColor: `${colors.teamPrimary}BF`,
+        },
+      ],
+    }
+
+    setData(chartData)
+  }, [])
+
+  if (!data) return <div />
+
+  return <Bar options={options} data={data} />
+}
+
+export default ActivityGraph
+
+// (property) options?: _DeepPartialObject<CoreChartOptions<"bar"> & ElementChartOptions<"bar"> & PluginChartOptions<"bar"> & DatasetChartOptions<"bar"> & ScaleChartOptions<...> & BarControllerChartOptions> | undefined
